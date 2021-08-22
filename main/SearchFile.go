f412c0293f9ec858dd8cec7e9e5176431a961faa
Dorapoketto <mxc2012@outlook.com>
2021-08-22 22:14:34 +0800

    Add

---
+++ main/SearchFile.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+var query = "test"
+var matches int
+
+func main() {
+	start := time.Now()
+	search("/Users/vincent/", true)
+	fmt.Println(matches, "matches")
+	fmt.Println(time.Since(start))
+}
+
+func search(path string, master bool) {
+	files, err := ioutil.ReadDir(path)
+	if err == nil {
+		for _, file := range files {
+			name := file.Name()
+			if name == query {
+				matches++
+			}
+			if file.IsDir() {
+				search(path+name+"/", false)
+			}
+		}
+	}
+}
