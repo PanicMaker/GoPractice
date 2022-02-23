191ee24c9c90381107d2b2d53d05569a13c40124
Dorapoketto <mxc2012@outlook.com>
2022-02-23 18:08:00 +0800

    Add: Gin的文件上传接口

--- web/main.go
+++ web/main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"path/filepath"
+	"time"
 )
 
 func main() {
-	r := gin.Default()
+	//r := gin.Default()
+	r := gin.New()
+
+	// LoggerWithFormatter 中间件会将日志写入 gin.DefaultWriter
+	// 默认 gin.DefaultWriter = os.Stdout
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// 你的自定义格式
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
+
+	r.MaxMultipartMemory = 8 << 20
+
 	r.GET("/", func(context *gin.Context) {
 		context.String(200, "Hello")
 	})
@@ -67,6 +94,35 @@ func main() {
 	r.GET("/goindex", func(c *gin.Context) {
 		c.Request.URL.Path = "/"
 		r.HandleContext(c)
+	})
+
+	// 单文件上传
+	r.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		filename := filepath.Base(file.Filename)
+		// 上传文件到指定的路径
+		c.SaveUploadedFile(file, filename)
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+	// 多文件上传
+	r.POST("/uploads", func(c *gin.Context) {
+		// 多文件
+		form, _ := c.MultipartForm()
+		files := form.File["files"]
+
+		for _, file := range files {
+			log.Println(file.Filename)
+
+			filename := filepath.Base(file.Filename)
+			//上传文件到指定的路径
+			c.SaveUploadedFile(file, filename)
+		}
+		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
 	})
 
 	r.Run(":9999")
