2820fe3a4a4a5805b544dc4b96a6890774977dbb
Dorapoketto <mxc2012@outlook.com>
2022-03-25 17:58:05 +0800

    Add: net 相关的一些代码，包括TCP和UDP 的使用

---
+++ net/tcp/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:8999")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go process(conn)
+	}
+}
+
+func process(conn net.Conn) {
+	defer conn.Close()
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+		n, err := reader.Read(buf[:])
+		if err != nil {
+			fmt.Println("read from client failed, err:", err)
+			break
+		}
+		recvStr := string(buf[:n])
+		fmt.Println("收到client端发来的数据：", recvStr)
+		conn.Write([]byte(recvStr)) // 发送数据
+	}
+}
