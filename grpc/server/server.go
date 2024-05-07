ba2ba6eb5f1d79a161e7f22e9c31b64a25c52bb9
PanicMaker <mxc2012@outlook.com>
2024-05-07 17:54:22 +0800

    Add: grpc 的简单应用

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -1,17 +1,18 @@
-// Package main implements a server for Greeter service.
 package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 
-	pb "github.com/PanicMaker/GoPractice/grpc/helloworld"
 	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-const (
-	port = ":50051"
+var (
+	port = flag.Int("port", 50051, "The server port")
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -26,12 +27,14 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
