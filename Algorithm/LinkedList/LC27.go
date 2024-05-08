feb7b81676daeda93dd2e50c93702946267b6a72
PanicMaker <mxc2012@outlook.com>
2024-05-08 23:33:58 +0800

    Add: LeetCode 27

---
+++ Algorithm/LinkedList/LC27.go
@@ -0,0 +1,16 @@
+package LinkedList
+
+// 27. 移除元素
+// https://leetcode.cn/problems/remove-element/description/
+
+func removeElement(nums []int, val int) int {
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
