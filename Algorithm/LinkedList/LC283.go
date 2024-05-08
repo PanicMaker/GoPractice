cd310bcd30e4b851465df16fff3f41f076681443
PanicMaker <mxc2012@outlook.com>
2024-05-08 23:34:06 +0800

    Add: LeetCode 283

---
+++ Algorithm/LinkedList/LC283.go
@@ -0,0 +1,19 @@
+package LinkedList
+
+// 283. 移动零
+// https://leetcode.cn/problems/move-zeroes/description/
+
+func moveZeroes(nums []int) {
+	slow, fast := 0, 0
+
+	for ; fast < len(nums); fast++ {
+		if nums[fast] != 0 {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+
+	for ; slow < len(nums); slow++ {
+		nums[slow] = 0
+	}
+}
