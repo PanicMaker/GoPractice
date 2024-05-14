21ed887f291537a0af79ea5122247cf4a71cf905
PanicMaker <mxc2012@outlook.com>
2024-05-14 15:59:55 +0800

    Update: LeetCode 5

--- Algorithm/LC5.go
+++ Algorithm/LC5.go
@@ -4,43 +4,75 @@ package main
 // https://leetcode.cn/problems/longest-palindromic-substring/description/
 
 // 动态规划解法
-func longestPalindrome(s string) string {
+func longestPalindrome1(s string) string {
 	n := len(s)
 	if n < 2 {
 		return s
 	}
 
-	maxLength := 1
-	begin := 0
+	maxLength := 1 // 最长回文子串的长度，至少为1
+	begin := 0     // 最长回文子串的起始位置
 
+	// 初始化动态规划数组
 	dp := make([][]bool, n)
 	for i := range dp {
 		dp[i] = make([]bool, n)
-		for j := range dp[i] {
-			dp[i][j] = false
-		}
-	}
-
-	for i := range dp {
+		// 单个字符一定是回文串
 		dp[i][i] = true
 	}
 
-	for i := 0; i < n; i++ {
-		for j := i; j >= 0; j-- {
+	// 从后往前遍历字符串
+	for i := n - 1; i >= 0; i-- {
+		// 从当前字符到字符串末尾遍历
+		for j := i; j < n; j++ {
 			if i == j {
+				// 单个字符一定是回文串
 				dp[i][j] = true
 			} else if j == i+1 {
+				// 两个相邻字符相同则是回文串
 				dp[i][j] = s[i] == s[j]
 			} else if s[i] == s[j] && dp[i+1][j-1] {
+				// 当前字符与末尾字符相同，并且去掉头尾后的子串也是回文串，则当前子串是回文串
 				dp[i][j] = true
 			}
 
 			if dp[i][j] && j-i+1 > maxLength {
+				// 如果当前子串是回文串，并且长度大于之前记录的最大长度，则更新最大长度和起始位置
 				maxLength = j - i + 1
 				begin = i
 			}
 		}
 	}
 
+	// 返回最长回文子串
 	return s[begin : begin+maxLength]
+}
+
+// 使用双指针解决
+func longestPalindrome2(s string) string {
+	// 在 s 中寻找以 s[l] 和 s[r] 为中心的最长回文串
+	palindrome := func(str string, l int, r int) string {
+		for l >= 0 && r < len(str) && str[l] == str[r] {
+			l--
+			r++
+		}
+		return str[l+1 : r]
+	}
+
+	res := ""
+	for i := 0; i < len(s); i++ {
+		// 奇数回文串
+		s1 := palindrome(s, i, i)
+		// 偶数回文串
+		s2 := palindrome(s, i, i+1)
+
+		if len(res) < len(s1) {
+			res = s1
+		}
+		if len(res) < len(s2) {
+			res = s2
+		}
+	}
+
+	return res
 }
---
+++ Algorithm/LC5_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func Test_longestPalindrome1(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1", args{s: "babad"}, "bab"},
+		{"2", args{s: "cbbd"}, "bb"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome1(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_longestPalindrome2(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1", args{s: "babad"}, "aba"},
+		{"2", args{s: "cbbd"}, "bb"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome2(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
