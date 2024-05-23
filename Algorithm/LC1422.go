bfee669f2428d386aeae6ce36496d2a8f58b714a
PanicMaker <mxc2012@outlook.com>
2024-05-23 17:08:50 +0800

    Add: LeetCode 1422

---
+++ Algorithm/LC1422.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"strings"
+)
+
+// 1422. 分割字符串的最大得分
+// https://leetcode.cn/problems/maximum-score-after-splitting-a-string/description/
+
+func maxScore(s string) int {
+	length := len(s)
+	res := 0
+
+	for i := 1; i < length; i++ {
+		sum := strings.Count(s[:i], "0") + strings.Count(s[i:], "1")
+		res = max(res, sum)
+	}
+
+	return res
+}
