69b41430bfee4211bccb57d53be18ef7b8e426f0
PanicMaker <mxc2012@outlook.com>
2024-06-05 21:49:55 +0800

    Add: 手写WEB框架 v0.2 实现上下文 Context 并拆分出路由 router.go

--- gee/gee.go
+++ gee/gee.go
@@ -1,25 +1,23 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+type HandlerFunc func(c *Context)
 
 type Engine struct {
-	routers map[string]HandlerFunc
+	router *router
 }
 
 func New() *Engine {
 	return &Engine{
-		routers: make(map[string]HandlerFunc),
+		router: newRouter(),
 	}
 }
 
 func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	e.routers[key] = handler
+	e.router.addRoute(method, pattern, handler)
 }
 
 func (e *Engine) GET(pattern string, handler HandlerFunc) {
@@ -27,7 +25,7 @@ func (e *Engine) GET(pattern string, handler HandlerFunc) {
 }
 
 func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRoute("Post", pattern, handler)
+	e.addRoute("POST", pattern, handler)
 }
 
 func (e *Engine) Run(addr string) error {
@@ -35,10 +33,6 @@ func (e *Engine) Run(addr string) error {
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	key := r.Method + "-" + r.URL.Path
-	if handler, ok := e.routers[key]; ok {
-		handler(w, r)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
-	}
+	c := newContext(w, r)
+	e.router.handle(c)
 }
--- gee/test/main.go
+++ gee/test/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"github.com/PanicMaker/GoPractice/gee"
 	"net/http"
+
+	"github.com/PanicMaker/GoPractice/gee"
 )
 
 func main() {
 	r := gee.New()
 
-	r.GET("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "URL.PATH = %q\n", r.URL.Path)
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
