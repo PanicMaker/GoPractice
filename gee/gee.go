30cf0e1f6bf2c5171e780c850ea29a4c789eac78
PanicMaker <mxc2012@outlook.com>
2024-06-10 18:11:11 +0800

    Add: 手写WEB框架 v0.4 新增 分组控制

--- gee/gee.go
+++ gee/gee.go
@@ -1,19 +1,27 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(c *Context)
 
 type Engine struct {
+	*RouteGroup
 	router *router
+	groups []*RouteGroup
 }
 
 func New() *Engine {
-	return &Engine{
+	engine := &Engine{
 		router: newRouter(),
 	}
+	engine.RouteGroup = &RouteGroup{
+		engine: engine,
+	}
+	engine.groups = []*RouteGroup{engine.RouteGroup}
+	return engine
 }
 
 func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
@@ -35,4 +43,34 @@ func (e *Engine) Run(addr string) error {
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := newContext(w, r)
 	e.router.handle(c)
+}
+
+type RouteGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	engine      *Engine
+}
+
+func (g *RouteGroup) Group(prefix string) *RouteGroup {
+	engine := g.engine
+	newGroup := &RouteGroup{
+		prefix: g.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (g *RouteGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
+}
+
+func (g *RouteGroup) GET(pattern string, handler HandlerFunc) {
+	g.addRoute("GET", pattern, handler)
+}
+
+func (g *RouteGroup) POST(pattern string, handler HandlerFunc) {
+	g.addRoute("POST", pattern, handler)
 }
