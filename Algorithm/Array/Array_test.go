2419a5f517d5c82a8c08afcaafde621dc2489d07
PanicMaker <mxc2012@outlook.com>
2024-06-14 17:54:50 +0800

    Add: LeetCode 80

--- Algorithm/Array/Array_test.go
+++ Algorithm/Array/Array_test.go
@@ -30,3 +30,27 @@ func TestLC88(t *testing.T) {
 		})
 	}
 }
+
+func TestLC80(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	var tests = []struct {
+		name   string
+		args   args
+		expect int
+	}{
+		{"1", args{[]int{1, 1, 1, 2, 2, 3}}, 5},
+		{"2", args{[]int{0, 0, 1, 1, 1, 1, 2, 3, 3}}, 7},
+		{"3", args{[]int{1}}, 1},
+		{"4", args{[]int{1, 1}}, 2},
+		{"5", args{[]int{1, 2}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates80II(tt.args.nums); !reflect.DeepEqual(got, tt.expect) {
+				t.Errorf("removeDuplicates80() = %v, expect %v", got, tt.expect)
+			}
+		})
+	}
+}
