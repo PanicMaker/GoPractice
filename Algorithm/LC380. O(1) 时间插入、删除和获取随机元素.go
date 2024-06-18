901e375b611c70e4d7ac3edb32aa591484c9ca42
PanicMaker <mxc2012@outlook.com>
2024-06-18 10:20:13 +0800

    Update: LeetCode 380 注释

--- Algorithm/LC380. O(1) 时间插入、删除和获取随机元素.go
+++ Algorithm/LC380. O(1) 时间插入、删除和获取随机元素.go
@@ -27,8 +27,8 @@ func (this *RandomizedSet) Insert(val int) bool {
 
 func (this *RandomizedSet) Remove(val int) bool {
 	if i, ok := this.maps[val]; ok {
-		this.array[i] = this.array[len(this.array)-1]
-		this.maps[this.array[i]] = i
+		this.array[i] = this.array[len(this.array)-1] // 将最后一个元素放到要删除的地方
+		this.maps[this.array[i]] = i                  // 更新原来最后一个元素在maps中存储的下标
 		delete(this.maps, val)
 		this.array = this.array[:len(this.array)-1]
 		return true
