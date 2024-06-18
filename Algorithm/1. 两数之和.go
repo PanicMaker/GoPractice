8f8331e0a459de8d9d58df6d7ad68777e4618046
PanicMaker <mxc2012@outlook.com>
2024-06-18 11:22:34 +0800

    Add: LeetCode 1

---
+++ Algorithm/1. 两数之和.go
@@ -0,0 +1,31 @@
+package main
+
+// https://leetcode.cn/problems/two-sum/description/
+
+// 两个循环
+func twoSumI(nums []int, target int) []int {
+	length := len(nums)
+
+	for i := 0; i < length; i++ {
+		for j := i + 1; j < length; j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// 使用哈希表
+func twoSumII(nums []int, target int) []int {
+	maps := make(map[int]int, len(nums))
+
+	for i, v := range nums {
+		if val, ok := maps[target-v]; ok {
+			return []int{i, val}
+		}
+		maps[v] = i
+	}
+
+	return nil
+}
