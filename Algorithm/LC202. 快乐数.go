53bdf124a067e032c63ad27cc75cea3f3ec7fb0e
PanicMaker <mxc2012@outlook.com>
2024-06-18 17:46:12 +0800

    Add: LeetCode 202

---
+++ Algorithm/LC202. 快乐数.go
@@ -0,0 +1,44 @@
+package main
+
+// https://leetcode.cn/problems/happy-number/description/
+
+// 通过一个哈希表记录数字是否重复出现
+func isHappyI(n int) bool {
+	step := func(n int) int {
+		sum := 0
+		for n > 0 {
+			sum += (n % 10) * (n % 10)
+			n /= 10
+		}
+		return sum
+	}
+
+	maps := make(map[int]bool)
+
+	for n != 1 && !maps[n] {
+		n, maps[n] = step(n), true
+	}
+
+	return n == 1
+}
+
+// 通过快慢指针判断，若不是快乐数，则会形成一个环
+func isHappyII(n int) bool {
+	step := func(n int) int {
+		sum := 0
+		for n > 0 {
+			sum += (n % 10) * (n % 10)
+			n /= 10
+		}
+		return sum
+	}
+
+	slow, fast := n, step(n)
+
+	for fast != 1 && slow != fast {
+		slow = step(slow)
+		fast = step(step(fast))
+	}
+
+	return fast == 1
+}
