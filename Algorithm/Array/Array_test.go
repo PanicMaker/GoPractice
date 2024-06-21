299ae4e7213c79c48f501f9d490917f2db6c7dc6
PanicMaker <mxc2012@outlook.com>
2024-06-21 16:45:11 +0800

    Add: LeetCode 238

--- Algorithm/Array/Array_test.go
+++ Algorithm/Array/Array_test.go
@@ -98,3 +98,47 @@ func TestLC189(t *testing.T) {
 		})
 	}
 }
+
+func TestLC167(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{2, 7, 11, 15}, 9}, []int{1, 2}},
+		{"1", args{[]int{2, 3, 4}, 6}, []int{1, 3}},
+		{"1", args{[]int{-1, 0}, -1}, []int{1, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLC238(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	var tests = []struct {
+		name   string
+		args   args
+		expect []int
+	}{
+		{"1", args{[]int{1, 2, 3, 4}}, []int{24, 12, 8, 6}},
+		{"2", args{[]int{-1, 1, 0, -3, 3}}, []int{0, 0, 9, 0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := productExceptSelfII(tt.args.nums); !reflect.DeepEqual(got, tt.expect) {
+				t.Errorf("productExceptSelfI() = %v, expect %v", got, tt.expect)
+			}
+		})
+	}
+}
