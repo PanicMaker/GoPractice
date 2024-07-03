d0a0878efbe67ee12a21c20f7f7b0f85831a6eb5
PanicMaker <mxc2012@outlook.com>
2024-07-03 23:24:21 +0800

    Update

--- Algorithm/Array/Array_test.go
+++ Algorithm/Array/Array_test.go
@@ -14,9 +14,9 @@ func TestLC15(t *testing.T) {
 		args   args
 		expect [][]int
 	}{
-		{"1", args{[]int{-1, 0, 1, 2, -1, -4}}, [][]int{[]int{-1, -1, 2}, []int{-1, 0, 1}}},
+		{"1", args{[]int{-1, 0, 1, 2, -1, -4}}, [][]int{{-1, -1, 2}, {-1, 0, 1}}},
 		{"2", args{[]int{0, 1, 1}}, [][]int{}},
-		{"3", args{[]int{0, 0, 0}}, [][]int{[]int{0, 0, 0}}},
+		{"3", args{[]int{0, 0, 0}}, [][]int{{0, 0, 0}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
