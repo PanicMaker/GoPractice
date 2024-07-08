478b052c6effa706cb0099f1644e9ec9be439cdc
PanicMaker <mxc2012@outlook.com>
2024-07-08 11:04:31 +0800

    Add: LeetCode 205

--- Algorithm/String/String_test.go
+++ Algorithm/String/String_test.go
@@ -211,3 +211,27 @@ func TestLC30(t *testing.T) {
 	}
 
 }
+
+func TestLC205(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{s: "egg", t: "add"}, true},
+		{"2", args{s: "foo", t: "bar"}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isIsomorphicII(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isIsomorphic() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
