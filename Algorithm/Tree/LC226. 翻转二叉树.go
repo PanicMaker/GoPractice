ceca04272e0551055817368d56a3d13868c08181
PanicMaker <mxc2012@outlook.com>
2024-07-16 00:35:54 +0800

    Add: LeetCode 226

---
+++ Algorithm/Tree/LC226. 翻转二叉树.go
@@ -0,0 +1,14 @@
+package Tree
+
+// https://leetcode.cn/problems/invert-binary-tree/
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
