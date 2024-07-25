df47c5351fd5f4d1ffa0422e76d69143c97420ab
PanicMaker <mxc2012@outlook.com>
2024-07-26 01:06:57 +0800

    Update: LeetCode 98

--- Algorithm/Tree/LC98. 验证二叉搜索树.go
+++ Algorithm/Tree/LC98. 验证二叉搜索树.go
@@ -49,3 +49,32 @@ func isValidBSTII(root *TreeNode) bool {
 
 	return check(root)
 }
+
+// 后序遍历
+func isValidBSTIII(root *TreeNode) bool {
+
+	var check func(node *TreeNode) (int, int)
+	check = func(node *TreeNode) (int, int) {
+		// 如果当前节点为空，返回最大整数和最小整数。这表示空子树对其父节点的判断没有任何影响
+		if node == nil {
+			return math.MaxInt, math.MinInt
+		}
+
+		lMin, lMax := check(node.Left)
+		rMin, rMax := check(node.Right)
+
+		// 如果当前节点的值小于等于左子树的最大值，或者大于等于右子树的最小值，那么当前子树不是BST
+		// 返回 math.MinInt 和 math.MaxInt 表示这个子树无效
+		if node.Val <= lMax || node.Val >= rMin {
+			return math.MinInt, math.MaxInt
+		}
+
+		// 返回当前子树的最小值和最大值，最小值是左子树的最小值和当前节点值中的较小者，最大值是右子树的最大值和当前节点值中的较大者
+		return min(lMin, node.Val), max(rMax, node.Val)
+	}
+
+	_, mx := check(root)
+
+	// 如果最大值不是 math.MaxInt，则树是一个有效的二叉搜索树
+	return mx != math.MaxInt
+}
