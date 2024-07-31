422d967a2a878b2ee003bd87f7c526b8a10bedc3
PanicMaker <mxc2012@outlook.com>
2024-07-31 16:02:05 +0800

    Update: LeetCode 2096

--- Algorithm/Tree/LC2096. 从二叉树一个节点到另一个节点每一步的方向.go
+++ Algorithm/Tree/LC2096. 从二叉树一个节点到另一个节点每一步的方向.go
@@ -6,8 +6,8 @@ import "strings"
 
 // 先找出公共祖先，再分别求出公共祖先到两个节点的路径，最后拼接在一起。会超出题目内存限制
 func getDirectionsI(root *TreeNode, startValue int, destValue int) string {
-	var dfs func(node *TreeNode, pVal int, qVal int) *TreeNode
-	dfs = func(node *TreeNode, pVal int, qVal int) *TreeNode {
+	var findLCA func(node *TreeNode, pVal int, qVal int) *TreeNode
+	findLCA = func(node *TreeNode, pVal int, qVal int) *TreeNode {
 		if node == nil {
 			return nil
 		}
@@ -16,8 +16,8 @@ func getDirectionsI(root *TreeNode, startValue int, destValue int) string {
 			return node
 		}
 
-		left := dfs(node.Left, pVal, qVal)
-		right := dfs(node.Right, pVal, qVal)
+		left := findLCA(node.Left, pVal, qVal)
+		right := findLCA(node.Right, pVal, qVal)
 
 		if left != nil && right != nil {
 			return node
@@ -30,7 +30,7 @@ func getDirectionsI(root *TreeNode, startValue int, destValue int) string {
 		return right
 	}
 
-	lca := dfs(root, startValue, destValue)
+	lca := findLCA(root, startValue, destValue)
 
 	var findPath func(node *TreeNode, target int, path string) string
 	findPath = func(node *TreeNode, target int, path string) string {
@@ -54,4 +54,54 @@ func getDirectionsI(root *TreeNode, startValue int, destValue int) string {
 	toD := findPath(lca, destValue, "")
 
 	return toS + toD
+}
+
+func getDirectionsII(root *TreeNode, startValue int, destValue int) string {
+	path := make([]byte, 0)
+
+	// 辅助函数，用于查找从当前节点到目标值的路径
+	var findPath func(node *TreeNode, target int) bool
+	findPath = func(node *TreeNode, target int) bool {
+		if node == nil {
+			return false
+		}
+
+		if node.Val == target {
+			return true
+		}
+
+		// 尝试左子树
+		path = append(path, 'L')
+		if findPath(node.Left, target) {
+			return true
+		}
+
+		// 如果在左子树中未找到，则尝试右子树
+		path[len(path)-1] = 'R'
+		if findPath(node.Right, target) {
+			return true
+		}
+
+		// 如果左右子树都未找到，回溯
+		path = path[:len(path)-1]
+		return false
+	}
+
+	// 查找从根节点到startValue的路径
+	findPath(root, startValue)
+	pathToStart := path
+
+	// 重置路径并查找从根节点到destValue的路径
+	path = nil
+	findPath(root, destValue)
+	pathToDest := path
+
+	// 去掉公共前缀部分
+	for len(pathToStart) > 0 && len(pathToDest) > 0 && pathToStart[0] == pathToDest[0] {
+		pathToStart = pathToStart[1:] // 去掉前缀相同的部分
+		pathToDest = pathToDest[1:]
+	}
+
+	// 构造最终路径: 'U'表示每次向上到最近公共祖先的步数，然后是到目标节点的剩余路径
+	return strings.Repeat("U", len(pathToStart)) + string(pathToDest)
 }
