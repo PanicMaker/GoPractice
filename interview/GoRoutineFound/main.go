11f6a4f9232f1faeddcd949e78a578ced15b4e51
PanicMaker <mxc2012@outlook.com>
2024-08-30 01:47:00 +0800

    Add: 多协程并发查找切片内的值

--- interview/GoRoutineFound/main.go
+++ interview/GoRoutineFound/main.go
@@ -1,23 +1,83 @@
 package main
 
-import "sync"
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	var wg sync.WaitGroup
-	n := 10
-
+	nums := []int{23, 32, 78, 43, 76, 65, 345, 762, 915, 86}
 	target := 5
 
-	for i := 0; i < n; i++ {
+	goroutineNum := 4
+	size := len(nums) / goroutineNum
+
+	var wg sync.WaitGroup
+	result := make(chan bool)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for i := 0; i < goroutineNum; i++ {
+		start := i * size
+		end := start + size
+		if i == len(nums)-1 {
+			end = len(nums)
+		}
 
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-		}()
-
+		go find(ctx, nums[start:end], target, result, &wg)
 	}
 
-	wg.Wait()
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
 
+	found := false
+	timeout := false
+
+	// 处理结果
+	for {
+		select {
+		case result, ok := <-result:
+			if !ok {
+				// channel 关闭
+				if found {
+					fmt.Println("Found it!")
+				} else if timeout {
+					fmt.Println("Timeout! Not Found")
+				}
+				return
+			}
+			if result {
+				found = true
+				cancel() // 取消所有 goroutine
+			}
+		case <-ctx.Done():
+			timeout = true
+			cancel() // 确保所有 goroutine 都被取消
+		}
+	}
+
+}
+
+func find(ctx context.Context, arr []int, target int, result chan<- bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, v := range arr {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if v == target {
+				result <- true
+				return
+			}
+		}
+	}
+
+	time.Sleep(5 * time.Second)
+	result <- false
 }
